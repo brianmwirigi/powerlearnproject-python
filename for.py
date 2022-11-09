baa3abc1ccbc190f7c8a6991687fcf4d1721bd12
mwirigi <bmmaingi17@gmail.com>
2022-11-09 21:11:37 +0300

    for loop

--- for.py
+++ for.py
@@ -1,6 +1,12 @@
 # will execute a set of statement
 
-music_genre = ["pop", "hip-hop", "jazz", "Gospel"]
+music_genre = ["pop", "hip-hop", "jazz", "Gospel"]  # list
 
 for music in music_genre:
     print(music)
+
+    if music == "jazz":
+        break  # loops out of the condition
+
+for i in "skinny":
+    print(i)
