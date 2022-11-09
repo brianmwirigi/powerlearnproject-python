b8f32ba400c17133145a215184ff19168a71170d
mwirigi <bmmaingi17@gmail.com>
2022-11-09 19:51:30 +0300

    control structures

---
+++ if-elif-else.py
@@ -0,0 +1,28 @@
+# if...elif...else statement
+
+# grading system
+
+# 80 - 100 ==> A
+# 70 - 70 ==> B
+# 60 - 69 ==> C
+# 50 - 59 ==> D
+# 0 - 49 ==> F
+
+marks = int(input("Enter marks"))
+if marks >= 80 and marks <= 100:  # concatination
+    print("Grade is A")
+
+elif marks >= 70 and marks <= 79:
+    print("grade is B")
+
+elif marks >= 60 and marks <= 69:
+    print("grade is C")
+
+elif marks >= 50 and marks <= 59:
+    print("grade is D")
+
+elif marks >= 0 and marks <= 49:
+    print("grade is FAIL")
+
+else:
+    print("out of the range")
--- intro.py
+++ intro.py
@@ -2,5 +2,9 @@ num1 = 0
 num2 = 1
 # relational operator (>,>, >=, <=, !=, ==)
 
-if num1 < num2:
+if num1 == num2:
+    # true block of code
     print('num1 is greater than num 2')
+else:
+    # false block of code
+    print('number 2 is greater than number 1')
