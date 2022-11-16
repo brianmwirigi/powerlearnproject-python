6748b72b61a3138c0e6a85792e9ba2fe8fa7f5db
mwirigi <bmmaingi17@gmail.com>
2022-11-16 18:50:58 +0300

    function

---
+++ function.py
@@ -0,0 +1,14 @@
+# function:
+# a block of code (set of statement) to perform a task
+# these block of are given a uniques name for identification
+
+# type
+# user defined function
+# in-built functions
+# __user defined function__
+# geneal syntax
+# def functionname(parameter):
+# statement to perform the task
+
+def greeting():
+    print("Jambo Africa. welcome to PLP")
